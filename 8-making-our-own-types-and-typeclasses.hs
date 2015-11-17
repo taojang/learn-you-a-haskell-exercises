5dd906b0124a4f4005fd6cf87ed810e73c209f8a
Tao Yang <tao.yang@zalando.de>
2015-11-17 02:16:51 +0100

    ex. 8

--- 8-making-our-own-types-and-typeclasses.hs
+++ 8-making-our-own-types-and-typeclasses.hs
@@ -12,13 +12,23 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+import qualified Data.List as L
+
+data Suit = Clubs | Diamonds | Hearts | Spades
+          deriving (Show, Eq, Ord, Bounded, Enum)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+           deriving (Show, Eq, Ord, Bounded, Enum)
+data Card = Card Digit Suit deriving (Eq, Ord, Bounded)
+
+instance Show Card where
+  show (Card digit suit) = "The " ++ (show digit) ++ " of " ++ (show suit)
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y
+  | x < y = y
+  | otherwise = x
+
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
@@ -27,13 +37,14 @@ class Hand a where
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play = any (== Card Ace Spades)
 
 -- Create a new Coin type
---data Coin = 
+data Coin = Head | Tail deriving (Eq, Show, Ord, Enum, Bounded)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+	play c = blah `L.isInfixOf`  c
+          where blah = take 10 $ repeat Head
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
